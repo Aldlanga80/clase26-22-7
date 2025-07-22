642ab4bde125932aba02b8a0655c0221f2d16a3d
Aldlanga80 <aldo.d.langa@gmail.com>
2025-07-22 17:23:55 -0300

    add:añadido estatico de register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,10 +2,29 @@ import { Layout } from "../components/Layout"
 
 const Register = () => {
   return (
-    <Layout background="red">
-      <h1>Hola desde Register</h1>
-    </Layout>
+    <Layout>
+      <h1>Registrate</h1>
 
+      <section>
+        <h2>Hola, bienvenido</h2>
+        <form>
+          <div>
+            <label>Username:</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Correo electrónico:</label>
+            <input type="email" />
+          </div>
+          <div>
+            <label>Contraseña:</label>
+            <input type="password" />
+          </div>
+          <button>Ingresar</button>
+        </form>
+      </section>
+    </Layout>
   )
 }
+
 export { Register }
