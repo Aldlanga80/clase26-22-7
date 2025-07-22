b6bb73adb5c15df5710a311099b8babbb24edd79
Aldlanga80 <aldo.d.langa@gmail.com>
2025-07-22 17:22:06 -0300

    add: estatico de form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,10 +2,25 @@ import { Layout } from "../components/Layout"
 
 const Login = () => {
   return (
-    <Layout background="red">
-      <h1>Hola desde Login</h1>
-    </Layout>
+    <Layout>
+      <h1>Inicia sesión</h1>
 
+      <section>
+        <h2>Hola, bienvenido de nuevo</h2>
+        <form>
+          <div>
+            <label>Correo electrónico:</label>
+            <input type="email" />
+          </div>
+          <div>
+            <label>Contraseña:</label>
+            <input type="password" />
+          </div>
+          <button>Ingresar</button>
+        </form>
+      </section>
+    </Layout>
   )
 }
+
 export { Login }
